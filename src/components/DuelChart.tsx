1c0ccf24ac08cf7ad27764ad6cc168957b7c92f5
sachinthacham <sachinthachamindu26@gmail.com>
2025-05-07 10:43:06 +0530

    update duelchart suitable for area and pie charts

--- src/components/DuelChart.tsx
+++ src/components/DuelChart.tsx
@@ -1,20 +1,29 @@
 import { useState } from "react";
-import BarChart from "../components/BarChart";
-import PieChart from "../components/PieChart";
+import HorizontalBarChartComponent from "../components/HorizontalBarChart";
+import PieChartComponent from "../components/PieChart";
 
 const DuelChart = () => {
-  const [selectedOption, setSelectedOption] = useState<string>("pie");
+  const [selectedOption, setSelectedOption] = useState<string>("area");
 
   const handleSetChart = (e: React.ChangeEvent<HTMLSelectElement>) => {
     setSelectedOption(e.target.value);
   };
   return (
-    <div className="w-full">
-      <select value={selectedOption} onChange={handleSetChart}>
-        <option value="bar">Bar chart</option>
-        <option value="pie">Pie chart</option>
+    <div className="w-full mx-5 pt-5">
+      <select
+        value={selectedOption}
+        onChange={handleSetChart}
+        className="w-1/4 bg-white text-gray-800 border border-gray-300 rounded-md h-10 px-3 shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 hover:border-blue-400 transition-colors"
+      >
+        <option value="area">📈 Area Chart</option>
+        <option value="pie">🥧 Pie Chart</option>
       </select>
-      {selectedOption === "bar" ? <BarChart /> : <PieChart />}
+
+      {selectedOption === "pie" ? (
+        <PieChartComponent />
+      ) : (
+        <HorizontalBarChartComponent />
+      )}
     </div>
   );
 };
