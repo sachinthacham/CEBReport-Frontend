39d9df0283a1d25e37c6fe3b3ce7e0fbd77d5645
sachinthacham <sachinthachamindu26@gmail.com>
2025-05-18 22:17:25 +0530

    import login and userdetails components

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import Reporting from "./pages/Reporting";
 import { Routes, Route } from "react-router-dom";
-import LandingPage from "./pages/LandingPage";
-import LoginLayout from "./components/LoginLayout";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import LoginPage from "./pages/LoginPage";
+import UserDetailsPageLayout from "./layouts/UserDetailsPageLayout";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/report" element={<Reporting />} />
-        <Route path="/" element={<LoginLayout />} />
-        <Route path="/landing" element={<LandingPage />} />
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/user" element={<UserDetailsPageLayout />} />
       </Routes>
       <ToastContainer />
     </>
