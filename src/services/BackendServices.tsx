7ac9bee45dce61820d81f3608fce6a0c3d2bfca3
sachinthacham <sachinthachamindu26@gmail.com>
2025-05-20 11:19:10 +0530

    implement get /post methods to fetch data

--- src/services/BackendServices.tsx
+++ src/services/BackendServices.tsx
@@ -1,7 +1,5 @@
 import axios from "axios";
-
 const BASE_URL = "http://localhost:5131/";
-
 export const fetchProvinceData = async () => {
   try {
     const response = await axios.get(`${BASE_URL}api/provinces/getall`);
@@ -29,5 +27,27 @@ export const fetchCustomerData = async () => {
     return response.data;
   } catch (error) {
     console.error("Error fetching data:", error);
+  }
+};
+
+export const fetchCustomerCountByProvince = async () => {
+  try {
+    const response = await axios.get(`${BASE_URL}api/Customers/provincecount`);
+    console.log(response.data);
+    return response.data;
+  } catch (error) {
+    console.log("Error fetching data:", error);
+  }
+};
+
+export const fetchCustomerCountByArea = async (provinceId: number) => {
+  try {
+    const response = await axios.get(
+      `${BASE_URL}api/Customers/areacount/${provinceId}`
+    );
+    console.log(response.data);
+    return response.data;
+  } catch (error) {
+    console.log("Error fetching data:", error);
   }
 };
