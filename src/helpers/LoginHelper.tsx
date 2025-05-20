d34bce74cad3afbb2b71cc3c7feb5a7a54251185
sachinthacham <sachinthachamindu26@gmail.com>
2025-05-20 11:16:14 +0530

    implement helper to post data

---
+++ src/helpers/LoginHelper.tsx
@@ -0,0 +1,8 @@
+export const postJSON = async (url: string, data: object) => {
+  const response = await fetch(url, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data),
+  });
+  return response.json();
+};
