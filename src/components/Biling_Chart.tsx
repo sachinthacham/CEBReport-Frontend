1d5f8eeefcb25bcf6e227f01b4df95ce02319cd0
sachinthacham <sachinthachamindu26@gmail.com>
2025-05-20 11:12:28 +0530

    implement chart for display billing details

---
+++ src/components/Biling_Chart.tsx
@@ -0,0 +1,45 @@
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
+
+type ReadDetails = {
+  billCycle: string;
+  days: string;
+  kwh: string;
+  readDate: string;
+  readMet1: string;
+  readMet2: string;
+  readMet3: string;
+  units: string;
+  year: string;
+};
+
+type Props = {
+  data: ReadDetails[];
+};
+
+const BillingChart = ({ data }: Props) => {
+  return (
+    <div className="w-full h-96">
+      <ResponsiveContainer width="100%" height="100%">
+        <BarChart data={data}>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="billCycle" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Bar dataKey="units" fill="#8884d8" radius={[4, 4, 0, 0]} />
+        </BarChart>
+      </ResponsiveContainer>
+    </div>
+  );
+};
+
+export default BillingChart;
