25b6ea4fc06c29750c06711b052318b6d763178c
sachinthacham <sachinthachamindu26@gmail.com>
2025-05-22 20:44:52 +0530

    remove the sidebar data array to  separate file

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import { Routes, Route } from "react-router-dom";
-import { ToastContainer } from "react-toastify";
 import Reporting from "./pages/Reporting";
 import LoginPage from "./pages/LoginPage";
+import { Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 import UserDetailsPageLayout from "./layouts/UserDetailsPageLayout";
 import Billing_payment_layout from "./layouts/Billing_payment_layout";
 import "react-toastify/dist/ReactToastify.css";
