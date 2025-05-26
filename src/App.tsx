d66b92ac9722a6feaec806dc3557c1bf67c23b2a
sachinthacham <sachinthachamindu26@gmail.com>
2025-05-26 23:13:34 +0530

    adding layout

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,27 @@ import LoginPage from "./pages/LoginPage";
 import { Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import UserDetailsPageLayout from "./layouts/UserDetailsPageLayout";
-import Billing_payment_layout from "./layouts/Billing_payment_layout";
+//import Billing_payment_layout from "./layouts/Layout";
 import "react-toastify/dist/ReactToastify.css";
+import Layout from "./layouts/Layout";
+import BillingPage from "./pages/Billing_payment";
+import UserLayout from "./Layout";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/report" element={<Reporting />} />
-        <Route path="/user" element={<UserDetailsPageLayout />} />
-        <Route path="/billing" element={<Billing_payment_layout />} />
+        {/* <Route path="/" element={<LoginPage />} /> */}
+        {/* <Route path="/report" element={<Reporting />} /> */}
+        {/* <Route path="/user" element={<UserDetailsPageLayout />} /> */}
+        <Route
+          path="/report"
+          element={
+            <UserLayout>
+              <Reporting />
+            </UserLayout>
+          }
+        />
       </Routes>
       <ToastContainer />
     </>
