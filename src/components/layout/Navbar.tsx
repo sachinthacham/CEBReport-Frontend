fdfc90d1fbf316d5a2714ccd85fd2d943e397429
sachinthacham <sachinthachamindu26@gmail.com>
2025-05-28 13:49:54 +0530

    removing unnessesery comments

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,3 +1,20 @@
+// import CEBlogo from "../../assets/CEB logo.png";
+
+// const Navbar = () => {
+//   return (
+//     <nav className="h-16 bg-white shadow-md flex items-center justify-between px-2 relative bg-fixed">
+//       <div className="flex justify-between items-center">
+//         {" "}
+//         <div>
+//           <img src={CEBlogo} alt="" width={100} height={100} />
+//         </div>
+//         <div className="text-xl font-bold text-[#800000]">CEB Reporting</div>
+//       </div>
+//       <div className=""></div>
+//     </nav>
+//   );
+// };
+// export default Navbar;
 import CEBlogo from "../../assets/CEB logo.png";
 import { FaArrowLeft } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
@@ -14,14 +31,17 @@ const Navbar = () => {
         <div>
           <img src={CEBlogo} alt="" width={100} height={100} />
         </div>
-        <div className="text-xl font-bold text-[#800000]">CEB Reporting</div>
+        <div className="text-xl font-bold text-[#800000]">CEB REPORTING</div>
       </div>
       <div className="">
         <div className="flex items-center gap-2 text-white bg-[#800000] hover:bg-[#a00000] px-4 py-2 rounded-md transition-colors cursor-pointer">
           <div>
             <FaArrowLeft />
           </div>
-          <div onClick={handleNavigate}> Back To Dashboard</div>
+          <div onClick={handleNavigate} className="text-sm">
+            {" "}
+            Back To Dashboard
+          </div>
         </div>
       </div>
     </nav>
