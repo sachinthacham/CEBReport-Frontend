533c741fec58df0cb52525d9fe9a95a0d892ddff
sachinthacham <sachinthachamindu26@gmail.com>
2025-06-03 12:27:16 +0530

    update the changes

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,16 @@ import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
 import { UserProvider } from "./contexts/UserContext.tsx";
 import { LoggedProvider } from "./contexts/UserLoggedStateContext.tsx";
+import { Provider } from "react-redux";
+import { store } from "./redux/Store.ts";
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <LoggedProvider>
       <UserProvider>
-        <App />
+        <Provider store={store}>
+          <App />
+        </Provider>
       </UserProvider>
     </LoggedProvider>
   </BrowserRouter>
