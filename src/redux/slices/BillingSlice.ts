f43cd3ab84b72320ca2ef7f64e8c92fb8f3e1608
sachinthacham <sachinthachamindu26@gmail.com>
2025-06-03 12:22:18 +0530

    implement billing slice

---
+++ src/redux/slices/BillingSlice.ts
@@ -0,0 +1,30 @@
+// src/redux/slices/billingSlice.ts
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+type BillingState = {
+  acctNo: string;
+  FbillCycle: number;
+  TbillCycle: number;
+};
+
+const initialState: BillingState = {
+  acctNo: "",
+  FbillCycle: 0,
+  TbillCycle: 0,
+};
+
+const billingSlice = createSlice({
+  name: "billing",
+  initialState,
+  reducers: {
+    setBillingData(state, action: PayloadAction<BillingState>) {
+      return { ...state, ...action.payload };
+    },
+    clearBillingData(state) {
+      return initialState;
+    },
+  },
+});
+
+export const { setBillingData, clearBillingData } = billingSlice.actions;
+export default billingSlice.reducer;
